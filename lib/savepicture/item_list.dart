192b4cb9d404ee361a673661d05bcff145392d31
Vũ Quang Nhật <101687608+vuquangnhat@users.noreply.github.com>
2023-11-01 16:36:50 +0700

    test thu kha nang hien thi detail item

--- lib/savepicture/item_list.dart
+++ lib/savepicture/item_list.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:test_thuetro/savepicture/item_details.dart';
 
 import 'package:test_thuetro/savepicture/savepicture.dart';
 
@@ -148,8 +149,8 @@ class ItemList extends StatelessWidget {
                             : Container(),
                       ),
                       onTap: () {
-                        // Navigator.of(context).push(MaterialPageRoute(
-                        //     builder: (context) => ItemDetails(thisItem['id'])));
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ItemDetails(thisItem['Post ID'])));
                       },
                     ),
                   );
