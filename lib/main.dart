50bd8e971325c4a519a13c69e4222691f1998c4c
Your Name~ <you@example.com>
2023-12-13 13:49:32 +0700

    add current user set

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,56 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-void main()async {
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(home: MainApp(),));
-    
+  runApp(MaterialApp(
+    home: MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
   CollectionReference user = FirebaseFirestore.instance.collection('Post');
+
   MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          
-      );
-      
-    
+    return Scaffold();
   }
-
-  
 }
+
+
+// Main in Chatapp
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//           useMaterial3: true,
+//         ),
+//         debugShowCheckedModeBanner: false,
+//         home: FutureBuilder(
+//             future: AuthMethods().getcurrentUser(),
+//             builder: (context, AsyncSnapshot<dynamic> snapshot){
+//               if(snapshot.hasData){
+//                 return ChatHome();
+//               }else{
+//                 return SignIn();
+//               }
+//             })
+//     );
+//   }
+// }
